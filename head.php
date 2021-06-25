1b32d0234bf5910df631e6af810f39eeaf512696
F537215 <raphael.pereira@foton.la>
2021-06-24 22:29:42 -0300

    retirada dos menus clientes, indicações e contação de histórias

--- head.php
+++ head.php
@@ -114,7 +114,7 @@
 							<li><a href="animacao_infantil">Animação Infantil</a></li>
 							<li><a href="animacao_oline">Animação Online</a></li>
 							<li><a href="camarim_fashion">Camarim Fashion</a></li>
-							<li><a href="contacao_de_historias">Contação de Histórias/Teatro</a></li>
+							<!-- <li><a href="contacao_de_historias">Contação de Histórias/Teatro</a></li> -->
 							<li><a href="oficina_de_slime">Oficina de Slime</a></li>
 							<li><a href="personagens_vivos">Personagens Vivos</a></li>
 							<li><a href="pintura_de_rosto">Pintura de Rosto</a></li>
@@ -136,8 +136,8 @@
 							<li><a href="eventos_anteriores">Eventos Anteriores</a></li>
 						</ul>				
 					</li>
-					<li><a href="alguns_clientes">Alguns Clientes</a></li>
-					<li><a href="indicacoes">Indicações</a></li>
+					<!-- <li><a href="alguns_clientes">Alguns Clientes</a></li> -->
+					<!-- <li><a href="indicacoes">Indicações</a></li> -->
 					<li><a href="contatos">Contatos</a></li>
 				</ul>
 			</nav>
